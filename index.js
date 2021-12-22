47f7f7561209f0e77efe1c53b9d7af9aa5b78dbe
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-12-22 19:08:14 +0700

    NodeJs Router

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const userRoute = require('./routes/user');
+
 const app = express();
 
 dotenv.config();
@@ -10,6 +12,9 @@ mongoose
     .then(() => console.log('DB Connect  Successfully'))
     .catch(err => console.log(err));
     
+app.use(express.json());
+
+app.use('/api/users', userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is running!");
