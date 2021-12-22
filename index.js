fad900a9240bc9ff3a786390d6d9b704aec423cd
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-12-22 18:23:59 +0700

    MongoDB Connection

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = express();
 
-app.listen(5000, () => {
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log('DB Connect  Successfully'))
+    .catch(err => console.log(err));
+    
+
+app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is running!");
 });
