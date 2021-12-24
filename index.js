7a70757f417a8966a7b39ca4407d01c5fe1822c8
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-12-24 16:35:40 +0700

    MongoDB Authentication

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoute = require('./routes/user');
+const authRoute = require('./routes/auth');
 
 const app = express();
 
@@ -14,6 +15,7 @@ mongoose
     
 app.use(express.json());
 
+app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
