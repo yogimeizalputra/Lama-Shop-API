74cf9bd69a0bdafdad5a5217cc42870096d0f4fc
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-12-26 16:29:25 +0700

    CRUD Operations With JWT

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
+const productRoute = require('./routes/product');
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
 
 const app = express();
 
@@ -17,6 +20,9 @@ app.use(express.json());
 
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
+app.use('/api/products', productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is running!");
